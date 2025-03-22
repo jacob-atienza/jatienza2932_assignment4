5609f18674de63d9e5216e2d5aaeb9d81d22b76c
jacob-atienza <jacobatienza14@gmail.com>
2025-03-22 02:54:59 -0400

    Add full CRUD for posts

---
+++ assign4/src/posts/views.py
@@ -0,0 +1,115 @@
+from django.shortcuts import render
+from .models import Post
+from django.http import JsonResponse
+from .forms import PostForm
+from profiles.models import Profile
+# Create your views here.
+
+def post_list_and_create(request):
+    form = PostForm(request.POST or None)
+
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        if form.is_valid():
+            author = Profile.objects.get(user=request.user)
+            instance = form.save(commit=False)
+            instance.author = author
+            instance.save()
+            return JsonResponse({
+                'title': instance.title,
+                'description': instance.description,
+                'author': instance.author.user.username,
+                'id': instance.id,
+            })
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/main.html', context)
+
+def post_detail(request, pk):
+    obj = Post.objects.get(pk=pk)
+    form = PostForm()
+
+    context = {
+        'obj': obj,
+        'form': form,
+    }
+
+    return render(request, 'posts/detail.html', context)
+
+def load_post_data_view(request, num_posts):
+    visible = 3
+    upper = num_posts
+    lower = upper - visible
+    size = Post.objects.all().count()
+
+    qs = Post.objects.all()
+    data = []
+    for obj in qs:
+        item = {
+            'id': obj.id,
+            'title': obj.title,
+            'description': obj.description,
+            'liked': True if request.user in obj.liked.all() else False,
+            'count': obj.like_count,
+            'author': obj.author.user.username,
+        }
+        data.append(item)
+    return JsonResponse({'data':data[lower:upper], 'size':size})
+
+def post_detail_data_view(request, pk):
+    obj = Post.objects.get(pk=pk)
+    data = {
+        'id': obj.id,
+        'title': obj.title,
+        'description': obj.description,
+        'author': obj.author.user.username,
+        'logged_in': request.user.username,
+    }
+    return JsonResponse({'data': data})
+
+def like_unlike_post(request):
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+
+        return JsonResponse({
+            'liked': liked,
+            'count': obj.liked.count()
+        })
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+def update_post(request, pk):
+    obj = Post.objects.get(pk=pk)
+
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        new_title = request.POST.get('title')
+        new_description = request.POST.get('description')
+
+        if not new_title or not new_description:
+            return JsonResponse({'error': 'Missing fields'}, status=400)
+
+        obj.title = new_title
+        obj.description = new_description
+        obj.save()
+
+        return JsonResponse({'title': new_title, 'description': new_description})
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+
+def delete_post(request, pk):
+    obj = Post.objects.get(pk=pk)
+
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        obj.delete()
+        return JsonResponse({'message': 'Post deleted successfully!'})
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
---
+++ assign4/src/static/functions.js
@@ -0,0 +1,6 @@
+const handleAlerts = (type, message) => {
+  alertBox.innerHTML = `
+    <div class="alert alert-${type}" role="alert">
+         ${message}
+    </div>`;
+};
